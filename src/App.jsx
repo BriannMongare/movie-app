9e34ec7cd241d2961937e67f6fcdd4751ad42a38
Brian Mong'are <reachbrianmongare@gmail.com>
2026-06-29 17:52:07 +0300

    Add SVG icons for search and star; configure Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+const App = () => {
+  return (
+   <>
+   <main>
+    <h1 className="text-3xl font-bold">Hello There</h1>
+    <p className="text-gray-300">lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+   </main>
+   </>
+  )
+}
+
+export default App
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx,html}',
+    './public/**/*.{html,js,svg,png}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
