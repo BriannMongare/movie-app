94f37c5163d0476c6f4ff6259e5c5e681470f0e3
Brian Mong'are <reachbrianmongare@gmail.com>
2026-07-28 01:15:31 +0300

    feat: integrate Appwrite for search tracking and trending movies

---
+++ src/appwrite.js
@@ -0,0 +1,53 @@
+import { Client, Databases, Query, ID } from "appwrite";
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+const client = new Client()
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(PROJECT_ID);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+try{
+    // 1. Use Appwrite SDK to check if a document with the given searchTerm already exists
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", searchTerm)
+    ]);
+
+    if (result.documents > 0) {
+      // Document exists, update the count
+      const doc = result.documents[0]; 
+
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count += 1,
+      });
+    } else {
+      // Document does not exist, create a new one
+      await database.createDocument(DATABASE_ID, COLLECTION_ID,ID.unique(), {
+        searchTerm:searchTerm,
+        count: 1,
+        movie_id: movie.id,
+        poster_url:`https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }           
+}catch (error) {
+    console.log("Error updating search count:", error);
+  }
+};
+
+export const getTrendingMovies = async () => {
+
+
+try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+        Query.limit(5),
+        Query.orderDesc("count"),
+    ]);
+    return result.documents;
+  } catch (error) {
+    console.log("Error fetching trending movies:", error);
+    return [];
+  } 
+};
